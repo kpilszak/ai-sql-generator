82b7e64379dadb0037b89a91c633b6dbefa871b8
kpilszak <klaudia.pilszak@gmail.com>
2025-12-02 06:04:12 -0500

    Fetch body message from frontent for getQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react'
 import CodeDisplay from "./components/CodeDisplay"
 import MessagesDisplay from "./components/MessagesDisplay"
 
+interface ChatData {
+  role: string,
+  content: string
+}
+
 const App = () => {
+  const [ value, setValue ] = useState<string>("")
+  const [ chat, setChat ] = useState<ChatData[]>([])
+  
   const getQuery = async () => {
     try {
       const options = {
@@ -10,22 +19,29 @@ const App = () => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          message: "create a table"
+          message: value
         })
       }
 
       const response = await fetch("http://localhost:8000/completions", options)
       const data = await response.json()
       console.log(data)
+      const userMessage = {
+        role: "user",
+        contant: value
+      }
+      setChat(oldChat => [...oldChat, data, userMessage])
     } catch (error) {
       console.error(error)
     }
   }
 
+  console.log(chat)
+
   return (
     <div className="app">
       <MessagesDisplay/>
-      <input/>
+      <input value={value} onChange={e => setValue(e.target.value)}/>
       <CodeDisplay/>
       <div className="button-container">
         <button id="get-query" onClick={getQuery}>Get Query!</button>
